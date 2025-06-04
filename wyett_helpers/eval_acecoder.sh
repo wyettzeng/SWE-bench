2e823a782bcfa27f62a3cb9e3a1915791c2f34db
wyett <wyettzeng@gmail.com>
2025-06-04 11:08:56 -0400

    update

--- wyett_helpers/eval_acecoder.sh
+++ wyett_helpers/eval_acecoder.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 python -m swebench.harness.run_evaluation \
-    --dataset_name princeton-nlp/SWE-bench_Lite \
-    --predictions_path <path_to_predictions> \
-    --max_workers <num_workers> \
+    --dataset_name princeton-nlp/SWE-bench_oracle \
+    --predictions_path "/home/wyett/SWE-bench/outputs/princeton-nlp__SWE-bench_oracle__test__TIGER-Lab__AceCoder-Qwen2.5-Coder-7B-Ins-V1.1__temp-0.0__top-p-1.0.jsonl" \
+    --max_workers 18 \
     --run_id "ace_coder"
     # use --predictions_path 'gold' to verify the gold patches
     # use --run_id to name the evaluation run
